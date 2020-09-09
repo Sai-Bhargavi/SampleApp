44cf5ec5e7d6d6caaead4b517bd1fc259dd2dc14
aires <SaiBhargavi.Aireni@in.pega.com>
2020-09-09 14:14:36 +0530

    intial commit

--- src/main/java/hello/world/LifeCycle.java
+++ src/main/java/hello/world/LifeCycle.java
@@ -1,31 +1,10 @@
-/*
- * $
- *
- * Copyright (c) 2020  Pegasystems Inc.
- * All rights reserved.
- *
- * This  software  has  been  provided pursuant  to  a  License
- * Agreement  containing  restrictions on  its  use.   The  software
- * contains  valuable  trade secrets and proprietary information  of
- * Pegasystems Inc and is protected by  federal   copyright law.  It
- * may  not be copied,  modified,  translated or distributed in  any
- * form or medium,  disclosed to third parties or used in any manner
- * not provided for in  said  License Agreement except with  written
- * authorization from Pegasystems Inc.
- */
 package hello.world;
-
 
 import io.micronaut.context.annotation.Context;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
-
-/**
- * @author vagrant
- * @version $ 9/9/20
- */
 
 @Context
 @Singleton
